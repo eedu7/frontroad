073f1864c9c02c22aaf4b0868a44807fde3fe2f6
eedu7 <mueedahmad067@gmail.com>
2025-04-24 22:13:02 +0500

    Changed the background color

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex min-h-screen flex-col">
             <Navbar />
-            <div className="flex-1">{children}</div>
+            <div className="flex-1 bg-[#F4F4F0]">{children}</div>
             <Footer />
         </div>
     );
