66090a38d42b080bcad0417c121b427beab87ad8
eedu7 <mueedahmad067@gmail.com>
2025-04-25 06:22:02 +0500

    added some changes

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -8,6 +8,12 @@ export default async function HomePage() {
 
     const data = await payload.find({
         collection: "categories",
+        depth: 1,
+        where: {
+            parent: {
+                exists: false,
+            },
+        },
     });
     return <div>{JSON.stringify(data, null, 2)}</div>;
 }
