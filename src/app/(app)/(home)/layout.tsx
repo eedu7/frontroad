1b206b0a6774304624908f5aa6c97c5e6388d154
eedu7 <mueedahmad067@gmail.com>
2025-04-25 06:50:58 +0500

    Added the search filters and search Input

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,11 +1,29 @@
 import { Footer } from "@/app/(app)/(home)/footer";
 import { Navbar } from "@/app/(app)/(home)/navbar";
+import { SearchFilters } from "@/app/(app)/(home)/search-filters";
+
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
 import React from "react";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function HomeLayout({ children }: { children: React.ReactNode }) {
+    const payload = await getPayload({
+        config: configPromise,
+    });
+
+    const data = await payload.find({
+        collection: "categories",
+        depth: 1,
+        where: {
+            parent: {
+                exists: false,
+            },
+        },
+    });
     return (
         <div className="flex min-h-screen flex-col">
             <Navbar />
+            <SearchFilters data={data} />
             <div className="flex-1 bg-[#F4F4F0]">{children}</div>
             <Footer />
         </div>
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,19 +1,3 @@
-import configPromise from "@payload-config";
-import { getPayload } from "payload";
-
-export default async function HomePage() {
-    const payload = await getPayload({
-        config: configPromise,
-    });
-
-    const data = await payload.find({
-        collection: "categories",
-        depth: 1,
-        where: {
-            parent: {
-                exists: false,
-            },
-        },
-    });
-    return <div>{JSON.stringify(data, null, 2)}</div>;
+export default function HomePage() {
+    return <div>Home Page</div>;
 }
