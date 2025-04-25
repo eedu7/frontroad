67daa1406fbeda4a2d4a8ab2a9ee698f5f42a328
eedu7 <mueedahmad067@gmail.com>
2025-04-25 18:30:12 +0500

    make the payload of the payloadjs global in the base Procedure

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
 export default function HomePage() {
-    return <div>Home Page</div>;
+    const trpc = useTRPC();
+    const categories = useQuery(trpc.categories.getMany.queryOptions());
+
+    return <div>{JSON.stringify(categories.data, null, 2)}</div>;
 }
