6c5772c2f1a5b0ad5add74e51d47401415196a49
eedu7 <mueedahmad067@gmail.com>
2025-04-26 13:46:00 +0500

    Added the generateAuthCookie method

---
+++ src/modules/auth/utils.ts
@@ -0,0 +1,17 @@
+import { cookies as getCookies } from "next/dist/server/request/cookies";
+
+interface Props {
+    prefix: string;
+    value: string;
+}
+
+export const generateAuthCookie = async ({ prefix, value }: Props) => {
+    const cookies = await getCookies();
+
+    cookies.set({
+        name: `${prefix}-token`, // payload-token
+        value: value,
+        httpOnly: true,
+        path: "/", // TODO: Ensure cross-domain cookie sharing
+    });
+};
