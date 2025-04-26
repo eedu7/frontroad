427c067b89da268a3872f96486a89236118b3da2
eedu7 <mueedahmad067@gmail.com>
2025-04-26 07:12:29 +0500

    Added the link towards home and sign in

--- src/modules/auth/server/procedures.ts
+++ src/modules/auth/server/procedures.ts
@@ -1,4 +1,5 @@
 import { AUTH_COOKIE } from "@/modules/auth/constants";
+import { registerSchema } from "@/modules/auth/schemas";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
 import { cookies as getCookies, headers as getHeaders } from "next/headers";
@@ -11,58 +12,41 @@ export const authRouter = createTRPCRouter({
         const session = await ctx.db.auth({ headers });
         return session;
     }),
-    register: baseProcedure
-        .input(
-            z.object({
-                email: z.string().email(),
-                password: z.string(),
-                username: z
-                    .string()
-                    .min(3, "Username must be at least 3 characters")
-                    .max(63, "Username must be less" + " than 63 characters")
-                    .regex(
-                        /^[a-z0-9][a-z0-9-]*[a-z0-9]$/,
-                        "Username can only contain lowercase letters, numbers and" +
-                            " hyphens. It must start and end with a letter or number",
-                    )
-                    .refine((val) => !val.includes("--"), "Username cannot contain consecutive hypens"),
-            }),
-        )
-        .mutation(async ({ ctx, input }) => {
-            await ctx.db.create({
-                collection: "users",
-                data: {
-                    email: input.email,
-                    username: input.username,
-                    password: input.password, // PayloadJs automatically handles this
-                },
+    register: baseProcedure.input(registerSchema).mutation(async ({ ctx, input }) => {
+        await ctx.db.create({
+            collection: "users",
+            data: {
+                email: input.email,
+                username: input.username,
+                password: input.password, // PayloadJs automatically handles this
+            },
+        });
+
+        const data = await ctx.db.login({
+            collection: "users",
+            data: {
+                email: input.email,
+                password: input.password,
+            },
+        });
+
+        if (!data.token) {
+            throw new TRPCError({
+                code: "UNAUTHORIZED",
+                message: "Failed to login",
             });
+        }
 
-            const data = await ctx.db.login({
-                collection: "users",
-                data: {
-                    email: input.email,
-                    password: input.password,
-                },
-            });
+        const cookies = await getCookies();
 
-            if (!data.token) {
-                throw new TRPCError({
-                    code: "UNAUTHORIZED",
-                    message: "Failed to login",
-                });
-            }
-
-            const cookies = await getCookies();
-
-            cookies.set({
-                name: AUTH_COOKIE,
-                value: data.token,
-                httpOnly: true,
-                path: "/",
-                // TODO: Ensure cross-domain cookie sharing
-            });
-        }),
+        cookies.set({
+            name: AUTH_COOKIE,
+            value: data.token,
+            httpOnly: true,
+            path: "/",
+            // TODO: Ensure cross-domain cookie sharing
+        });
+    }),
     login: baseProcedure
         .input(
             z.object({
--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -1,9 +1,64 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+import { registerSchema } from "@/modules/auth/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["700"],
+});
 
 export const SignUpView = () => {
+    const form = useForm<z.infer<typeof registerSchema>>({
+        resolver: zodResolver(registerSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+            username: "",
+        },
+    });
+
+    const onSubmit = (values: z.infer<typeof registerSchema>) => {
+        console.log(values);
+    };
+
     return (
         <div className="grid grid-cols-1 lg:grid-cols-5">
-            <div className="h-screen w-full overflow-y-auto bg-[#F4F4F0] lg:col-span-3">Form Column</div>
+            <div className="h-screen w-full overflow-y-auto bg-[#F4F4F0] lg:col-span-3">
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="flex flex-col gap-8 p-4 lg:p-16"
+                    >
+                        <div className="mb-8 flex items-center justify-between">
+                            <Link href={"/"}>
+                                <span className={cn("text-2xl font-semibold", poppins.className)}>frontroad</span>
+                            </Link>
+                            <Button
+                                asChild
+                                variant="ghost"
+                                size="sm"
+                                className="border-none text-base underline"
+                            >
+                                <Link
+                                    prefetch
+                                    href={"/sign-in"}
+                                >
+                                    Sign in
+                                </Link>
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+            </div>
             <div
                 style={{
                     backgroundImage: "url('/auth-bg.png')",
