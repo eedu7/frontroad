faef4d013ba66a59d20108852824394858215002
eedu7 <mueedahmad067@gmail.com>
2025-04-26 14:58:30 +0500

    Changed the background color

--- src/modules/home/ui/components/search-filters/index.tsx
+++ src/modules/home/ui/components/search-filters/index.tsx
@@ -1,21 +1,38 @@
 "use client";
 
+import { DEFAULT_BG_COLOR } from "@/modules/home/constants";
 import { Categories } from "@/modules/home/ui/components/search-filters/categories";
 import { SearchInput } from "@/modules/home/ui/components/search-filters/search-input";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
 
 import React from "react";
 
 export const SearchFilters = () => {
+    const params = useParams();
+
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
+
+    const categoryParam = params?.category as string | undefined;
+    const activeCategory = categoryParam || "all";
+
+    const activeCategoryData = data.find((category) => category.slug === activeCategory);
+
+    const activeCategoryColor = activeCategoryData?.color || DEFAULT_BG_COLOR;
+    const activeCategoryName = activeCategoryData?.name || null;
+
+    const activeSubcategory = params.subcategory as string | undefined;
+
+    const activeSubcategoryName =
+        activeCategoryData?.subcategories?.find((subcategory) => subcategory.slug === activeSubcategory)?.name || null;
 
     return (
         <div
             className="flex w-full flex-col gap-4 border-b px-4 py-8 lg:px-12"
             style={{
-                backgroundColor: "#F5F5F5",
+                backgroundColor: activeCategoryColor,
             }}
         >
             <SearchInput />
