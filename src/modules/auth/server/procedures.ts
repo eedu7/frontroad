b58724b5510589890e58d0d693ef7206c7ef55c8
eedu7 <mueedahmad067@gmail.com>
2025-04-26 06:12:11 +0500

    Added the auth router

---
+++ src/modules/auth/server/procedures.ts
@@ -0,0 +1,11 @@
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { headers as getHeaders } from "next/headers";
+
+export const authRouter = createTRPCRouter({
+    session: baseProcedure.query(async ({ ctx }) => {
+        const headers = await getHeaders();
+
+        const session = await ctx.db.auth({ headers });
+        return session;
+    }),
+});
