b761fe182021ec56b168123d804e7fc8c30d7730
eedu7 <mueedahmad067@gmail.com>
2025-04-29 11:54:05 +0500

    Added the product router procude and its type

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,27 +1,12 @@
-import { Category } from "@/payload-types";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 
 export const productsRouter = createTRPCRouter({
     getMany: baseProcedure.query(async ({ ctx }) => {
         const data = await ctx.db.find({
-            collection: "categories",
-            depth: 1,
-            pagination: false,
-            where: {
-                parent: {
-                    exists: false,
-                },
-            },
-            sort: "name",
+            collection: "products",
+            depth: 1, // Populate "category" & "image
         });
 
-        return data.docs.map((doc) => ({
-            ...doc,
-            subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
-                // Because of "depth: 1", we are confident "doc" will be of type "Categpry"
-                ...(doc as Category),
-                subcategories: undefined,
-            })),
-        }));
+        return data;
     }),
 });
