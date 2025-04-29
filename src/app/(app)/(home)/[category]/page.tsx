e8da1fa0e4ed7da89ad3372cb25e97d9a90d4ba5
eedu7 <mueedahmad067@gmail.com>
2025-04-29 12:08:44 +0500

    Added the prefetching

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import { ProductList } from "@/modules/products/ui/components/product-list";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 interface Props {
     params: Promise<{ category: string }>;
 }
 
 export default async function CategoryPage({ params }: Props) {
-    const { category } = await params;
+    const queryClient = getQueryClient();
 
-    return <div>Category: {category}</div>;
+    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions());
+
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<ProductListSkeleton />}>
+                <ProductList />
+            </Suspense>
+        </HydrationBoundary>
+    );
 }
+
+const ProductListSkeleton = () => {
+    return <p>Loading...</p>;
+};
---
+++ src/modules/products/ui/components/product-list.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import React from "react";
+
+export const ProductList = () => {
+    const trpc = useTRPC();
+    const { data } = useSuspenseQuery(trpc.products.getMany.queryOptions());
+
+    return <div>{JSON.stringify(data, null, 2)}</div>;
+};
