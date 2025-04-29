c9d69a61dab5fbb4b6a876913a0a069eb9009ef0
eedu7 <mueedahmad067@gmail.com>
2025-04-29 18:31:07 +0500

    fixed

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -13,16 +13,22 @@ export const productsRouter = createTRPCRouter({
             }),
         )
         .query(async ({ ctx, input }) => {
-            const where: Where = {};
+            const where: Where = {
+                price: {},
+            };
 
-            if (input.minPrice) {
-                where.price = {
-                    greater_than_equal: input.minPrice,
-                };
-            }
-            if (input.maxPrice) {
+            if (input.minPrice && input.maxPrice) {
                 where.price = {
                     less_than_equal: input.maxPrice,
+                    greater_than_equal: input.minPrice,
+                };
+            } else if (input.minPrice) {
+                where.price = {
+                    less_than_equal: input.maxPrice,
+                };
+            } else if (input.maxPrice) {
+                where.price = {
+                    greater_than_equal: input.minPrice,
                 };
             }
 
