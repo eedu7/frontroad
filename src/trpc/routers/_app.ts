059ed69d4085ab4aa82803de7af3f11b6e76a5ab
eedu7 <mueedahmad067@gmail.com>
2025-04-30 11:10:58 +0500

    Added the tags Router

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,12 +1,14 @@
 import { authRouter } from "@/modules/auth/server/procedures";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { productsRouter } from "@/modules/products/server/procedures";
+import { tagsRouter } from "@/modules/tags/server/procedures";
 import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
     categories: categoriesRouter,
     auth: authRouter,
     products: productsRouter,
+    tags: tagsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
