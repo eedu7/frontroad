adbdcfb6ce78e7c0c4265ae5813a33c5be5759ab
eedu7 <mueedahmad067@gmail.com>
2025-04-30 21:49:20 +0500

    Added the tenantSlug

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -16,6 +16,7 @@ export const productsRouter = createTRPCRouter({
                 maxPrice: z.string().nullable().optional(),
                 tags: z.array(z.string()).nullable().optional(),
                 sort: z.enum(sortValues).nullable().optional(),
+                tenantSlug: z.string().nullable().optional(),
             }),
         )
         .query(async ({ ctx, input }) => {
@@ -45,6 +46,12 @@ export const productsRouter = createTRPCRouter({
             } else if (input.maxPrice) {
                 where.price = {
                     less_than_equal: input.maxPrice,
+                };
+            }
+
+            if (input.tenantSlug) {
+                where["tenant.slug"] = {
+                    equals: input.tenantSlug,
                 };
             }
 
