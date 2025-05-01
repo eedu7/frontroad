c5e324ce6575cdfc164b7328a95fd43e85dde9a9
eedu7 <mueedahmad067@gmail.com>
2025-05-01 10:13:26 +0500

    Added the use-cart-store

---
+++ src/modules/checkout/store/use-cart-store.ts
@@ -0,0 +1,57 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+interface TenantCard {
+    productsId: string[];
+}
+
+interface CartState {
+    tenantCarts: Record<string, TenantCard>;
+    addProduct: (tenantSlug: string, productId: string) => void;
+    removeProduct: (tenantSlug: string, productId: string) => void;
+    clearCart: (tenantSlug: string) => void;
+    clearAllCarts: () => void;
+    getCartByTenant: (tenantSlug: string) => string[];
+}
+
+export const useCartStore = create<CartState>()(
+    persist(
+        (set, get) => ({
+            tenantCarts: {},
+            addProduct: (tenantSlug, productId) =>
+                set((state) => ({
+                    tenantCarts: {
+                        ...state.tenantCarts,
+                        [tenantSlug]: {
+                            productsId: [...(state.tenantCarts[tenantSlug]?.productsId || []), productId],
+                        },
+                    },
+                })),
+            removeProduct: (tenantSlug, productId) =>
+                set((state) => ({
+                    tenantCarts: {
+                        ...state.tenantCarts,
+                        [tenantSlug]: {
+                            productsId: [...(state.tenantCarts[tenantSlug]?.productsId || []), productId],
+                        },
+                    },
+                })),
+
+            clearCart: (tenantSlug) =>
+                set((state) => ({
+                    tenantCarts: {
+                        ...state.tenantCarts,
+                        [tenantSlug]: {
+                            productsId: [],
+                        },
+                    },
+                })),
+            clearAllCarts: () => set({ tenantCarts: {} }),
+            getCartByTenant: (tenantSlug: string) => get().tenantCarts[tenantSlug]?.productsId || [],
+        }),
+        {
+            name: "funroad-cart",
+            storage: createJSONStorage(() => localStorage),
+        },
+    ),
+);
