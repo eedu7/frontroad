2cc4e709f9682da8a7435af9c958cf6b45b0cb2f
eedu7 <mueedahmad067@gmail.com>
2025-05-02 06:35:09 +0500

    Created the specfic library page

---
+++ src/app/(app)/(library)/library/[productId]/page.tsx
@@ -0,0 +1,25 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import React from "react";
+
+interface Props {
+    params: Promise<{ productId: string }>;
+}
+
+export default async function LibraryIDPage({ params }: Props) {
+    const { productId } = await params;
+
+    const queryClient = getQueryClient();
+
+    void queryClient.prefetchQuery(
+        trpc.library.getOne.queryOptions({
+            productId,
+        }),
+    );
+
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <ProductView />
+        </HydrationBoundary>
+    );
+}
