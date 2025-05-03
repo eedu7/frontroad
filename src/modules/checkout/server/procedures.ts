f26ac1e606b1c1cc19f6b3a8b8bd06b420e82245
eedu7 <mueedahmad067@gmail.com>
2025-05-03 14:45:07 +0500

    Updated isArchived fields

--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -76,6 +76,11 @@ export const checkoutRouter = createTRPCRouter({
                                 equals: input.tenantSlug,
                             },
                         },
+                        {
+                            isArchived: {
+                                not_equals: true,
+                            },
+                        },
                     ],
                 },
             });
@@ -179,9 +184,18 @@ export const checkoutRouter = createTRPCRouter({
                 collection: "products",
                 depth: 2, // Populate "category", "image" and "tenant" + "tenant + image"
                 where: {
-                    id: {
-                        in: input.ids,
-                    },
+                    and: [
+                        {
+                            id: {
+                                in: input.ids,
+                            },
+                        },
+                        {
+                            isArchived: {
+                                not_equals: true,
+                            },
+                        },
+                    ],
                 },
             });
 
