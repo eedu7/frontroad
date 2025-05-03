58e3a62ff31b345cbb1cc8a888a8c5b38d8fffd2
eedu7 <mueedahmad067@gmail.com>
2025-05-03 12:32:16 +0500

    Added the verify stripe procedure

--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -7,6 +7,51 @@ import type Stripe from "stripe";
 import { z } from "zod";
 
 export const checkoutRouter = createTRPCRouter({
+    verify: protectedProcedure.mutation(async ({ ctx }) => {
+        const user = await ctx.db.findByID({
+            collection: "users",
+            id: ctx.session.user.id,
+            depth: 0,
+        });
+
+        if (!user) {
+            throw new TRPCError({
+                code: "NOT_FOUND",
+                message: "User not found",
+            });
+        }
+
+        const tenantId = user.tenants?.[0]?.tenant as string; // This is an id because of depth: 0
+        const tenant = await ctx.db.findByID({
+            collection: "tenants",
+            id: tenantId,
+        });
+
+        if (!tenant) {
+            throw new TRPCError({
+                code: "NOT_FOUND",
+                message: "Tenant not found",
+            });
+        }
+
+        const accountLink = await stripe.accountLinks.create({
+            account: tenant.stripeAccountId,
+            refresh_url: `${process.env.NEXT_PUBLIC_APP_URL!}/admin`,
+            return_url: `${process.env.NEXT_PUBLIC_APP_URL!}/admin`,
+            type: "account_onboarding",
+        });
+
+        if (!accountLink.url) {
+            throw new TRPCError({
+                code: "BAD_REQUEST",
+                message: "Failed to create verification link",
+            });
+        }
+
+        return {
+            url: accountLink.url,
+        };
+    }),
     purchase: protectedProcedure
         .input(
             z.object({
