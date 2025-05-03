db68e00c785b05ca9af4773af7757730727cace2
eedu7 <mueedahmad067@gmail.com>
2025-05-03 14:39:23 +0500

    Added the productview skeleton

--- src/app/(app)/(tenants)/tenants/[slug]/(home)/products/[productId]/page.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/products/[productId]/page.tsx
@@ -1,7 +1,7 @@
-import { ProductView } from "@/modules/products/ui/views/product-view";
+import { ProductView, ProductViewSkeleton } from "@/modules/products/ui/views/product-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import React from "react";
+import React, { Suspense } from "react";
 
 interface Props {
     params: Promise<{
@@ -21,10 +21,12 @@ export default async function ProductPage({ params }: Props) {
 
     return (
         <HydrationBoundary state={dehydrate(queryClient)}>
-            <ProductView
-                productId={productId}
-                tenantSlug={slug}
-            />
+            <Suspense fallback={<ProductViewSkeleton />}>
+                <ProductView
+                    productId={productId}
+                    tenantSlug={slug}
+                />
+            </Suspense>
         </HydrationBoundary>
     );
 }
--- src/modules/products/ui/views/product-view.tsx
+++ src/modules/products/ui/views/product-view.tsx
@@ -173,3 +173,20 @@ export const ProductView = ({ productId, tenantSlug }: Props) => {
         </div>
     );
 };
+
+export const ProductViewSkeleton = () => {
+    return (
+        <div className="px-4 py-10 lg:px-12">
+            <div className="overflow-hidden rounded-sm border bg-white">
+                <div className="relative aspect-[3.9] border-b">
+                    <Image
+                        src={"/placeholder.svg"}
+                        alt={"Placeholder"}
+                        fill
+                        className="object-cover"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
