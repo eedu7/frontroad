e4eb290001aba9e4177827f5f763b9e96de0bc43
eedu7 <mueedahmad067@gmail.com>
2025-05-06 12:52:33 +0500

    Removed the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,16 +14,16 @@ export const config = {
 };
 
 export default async function middleware(req: NextRequest) {
-    const url = req.nextUrl;
-    // Extract the hostname (e.g "eedu7.frontroad.com")
-    const hostname = req.headers.get("host") || ";";
-
-    const rootDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN;
-
-    if (hostname.endsWith(`.${rootDomain}`)) {
-        const tenantSlug = hostname.replace(`.${rootDomain}`, "");
-        return NextResponse.rewrite(new URL(`/tenants/${tenantSlug}${url.pathname}`, req.url));
-    }
+    // const url = req.nextUrl;
+    // // Extract the hostname (e.g "eedu7.frontroad.com")
+    // const hostname = req.headers.get("host") || ";";
+    //
+    // const rootDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN;
+    //
+    // if (hostname.endsWith(`.${rootDomain}`)) {
+    //     const tenantSlug = hostname.replace(`.${rootDomain}`, "");
+    //     return NextResponse.rewrite(new URL(`/tenants/${tenantSlug}${url.pathname}`, req.url));
+    // }
 
     return NextResponse.next();
 }
