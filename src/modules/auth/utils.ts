32cbd127feeef680c15857388e35794a39b5bb11
eedu7 <mueedahmad067@gmail.com>
2025-05-06 11:53:07 +0500

    Modifiy the generateAuthCookie

--- src/modules/auth/utils.ts
+++ src/modules/auth/utils.ts
@@ -12,6 +12,9 @@ export const generateAuthCookie = async ({ prefix, value }: Props) => {
         name: `${prefix}-token`, // payload-token
         value: value,
         httpOnly: true,
-        path: "/", // TODO: Ensure cross-domain cookie sharing
+        path: "/",
+        sameSite: "none",
+        domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
+        secure: process.env.NODE_ENV === "production",
     });
 };
